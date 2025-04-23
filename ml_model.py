cb3c9568b4fa48b05eb123a474abdf9bc4d77b91
Mohamed-s-negm <msen6639@gmail.com>
2025-04-24 00:46:36 +0300

    #11 Added the comments to understand the use of every part of the class.

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,4 @@
+#We import the libraries needed
 import pandas as pd
 import numpy as np
 import pickle
@@ -7,7 +8,10 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 
+#Create a machine learning class for the model
 class ML_Model:
+
+    #define whether we have an already built-in model or just add the dataset path
     def __init__(self, data_path = None, model_file = None):
         self.data_path = data_path
         self.model_file = model_file
@@ -16,11 +20,13 @@ class ML_Model:
         self.predictions = None
         self.start_evaluate = False
 
+        #We check for the data_path and model_file if exists
         if data_path:
             self.load_split_data(data_path)
         if model_file:
             self.load_model(model_file)
 
+    #We prepare the data to be handled by the model
     def load_split_data(self, path):
         df = pd.read_csv(path)
         X = df.drop(columns='State')
@@ -29,6 +35,7 @@ class ML_Model:
             X, y, test_size=0.3, random_state=101
         )
 
+    #Function for the random_forest model
     def random_forest(self):
         print('Training using Random Forest...')
         self.model = RandomForestClassifier(n_estimators=100)
@@ -39,6 +46,7 @@ class ML_Model:
         self.save_model(self.model_file)
         self.evaluate()
 
+    #Funtion for decision_tree model
     def decision_tree(self):
         print('Training using Decision Tree...')
         self.model = DecisionTreeClassifier()
@@ -49,6 +57,7 @@ class ML_Model:
         self.save_model(self.model_file)
         self.evaluate()
 
+    #Function to evaluate the model and it's automatically called after training the model
     def evaluate(self):
         if self.model == None:
             print('No models has been trained.')
@@ -60,12 +69,14 @@ class ML_Model:
         print('Classification Report: ', classification_report(self.y_test, self.predictions))
         print('Confusion Matrix: ', confusion_matrix(self.y_test, self.predictions))
 
+    #Function to make the predictions depending on the input data
     def predict(self, new_data):
         if self.model is None:
             raise Exception("No model loaded. Use load_model() first.")
 
         return self.model.predict(new_data)
     
+    #Function to save the created model and it's called right after creating a model
     def save_model(self, filename):
         if self.model is None:
             raise Exception("No trained model to save.")
@@ -73,6 +84,7 @@ class ML_Model:
         with open(filename, 'wb') as f:
             pickle.dump(self.model, f)
 
+    #Function to load an existing model
     def load_model(self, filename):
         if os.path.exists(filename):
             with open(filename, 'rb') as f:
